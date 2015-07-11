37ff1ca880a85ea6c77d03244fbd62e5d543a566
Matt Gaunt <matt@gauntface.co.uk>
2015-07-11 19:46:26 +0100

    Adding drag and drop support

--- src/scripts/blog-editor/blog-editor-controller.es6.js
+++ src/scripts/blog-editor/blog-editor-controller.es6.js
@@ -36,11 +36,56 @@ class BlogEditorController extends BaseController {
     console.log('File Dropped: ', file);
 
     // Upload File
-    
+    this.uploadFile(file)
+      .then(function(imgUrl) {
+        console.log('Loaded the image to: ', imgUrl);
+      })
+      .catch(function(err) {
+        console.error('Unable to load the image: ', err);
+      });
     // Show UI During Upload
 
     // Show UI for what to do with the Image
 
+  }
+
+  uploadFile(file) {
+    return new Promise(function(resolve, reject) {
+      var formData = new FormData();
+      formData.append('file', file);
+
+      // now post a new XHR request
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '');
+      xhr.onreadystatechange = function(e) {
+        if (e.target.readyState !== 4) {
+          return;
+        }
+
+        var response;
+        if (e.target.status === 200) {
+          response = JSON.parse(e.target.responseText);
+          var imgUrl = response.data.url;
+          resolve(imgUrl);
+        } else {
+          console.log(e.target.responseText);
+          try {
+            response = JSON.parse(e.target.responseText);
+            reject(response.error.msg);
+          } catch (exception) {
+            reject('Sorry, the server responded with status code: ' +
+              e.target.status + ' response: ' + e.target.responseText);
+          }
+        }
+      }.bind(this);
+
+      xhr.timeout = 10000;
+      xhr.ontimeout = function() {
+        reject('Sorry, the server couldn\'t be reached.');
+      };
+
+      xhr.send(formData);
+    });
   }
 }
 
