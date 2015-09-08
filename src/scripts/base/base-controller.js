45c04dafb34ee5ed0ecca9998025e13c13b151e8
Matt Gaunt <matt@gauntface.co.uk>
2015-09-08 14:56:36 +0100

    Adding in the window load rule for async CSS

--- src/scripts/base/base-controller.js
+++ src/scripts/base/base-controller.js
@@ -7,13 +7,16 @@ var LoadCSS = require('./../third_party/loadCSS/loadCSS.js');
 export default class BaseController {
   constructor() {
     addAnalytics();
-    this.asyncLoadCSS();
 
     window.GauntFace = window.GauntFace || {};
 
     if (this.onDOMContentLoaded) {
       this.addDOMContentLoadedCallback(() => this.onDOMContentLoaded());
     }
+
+    window.addEventListener('load', () => {
+      this.asyncLoadCSS();
+    });
   }
 
   asyncLoadCSS() {
