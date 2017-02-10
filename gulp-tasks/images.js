449bce947c66b838cf3218481859e6f9ade8b203
Matt Gaunt <matt@gauntface.co.uk>
2017-02-09 17:13:37 -0800

    Adding imagemin to the build process

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -3,11 +3,26 @@
 /* eslint-env node */
 
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('images', () => {
+gulp.task('images:copy', () => {
   return gulp.src([
-    global.config.src + '/**/*.{jpg,png,svg,ico}',
+    global.config.src + '/**/*.{svg,ico}',
     `!${global.config.src}/server/libraries/**/*`,
   ])
   .pipe(gulp.dest(global.config.dest));
 });
+
+gulp.task('images:minified', () => {
+  return gulp.src([
+    global.config.src + '/**/*.{jpg,jpeg,png}',
+    `!${global.config.src}/server/libraries/**/*`,
+  ])
+  .pipe(imagemin())
+  .pipe(gulp.dest(global.config.dest));
+});
+
+gulp.task('images', gulp.parallel(
+  'images:copy',
+  'images:minified'
+));
