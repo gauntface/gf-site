774f0984392c87184b2c6efa5113b6d70e55f34f
Matt Gaunt <matt@gauntface.co.uk>
2017-06-08 22:10:25 -0700

    give more time

--- test/node/home-blog-posts.js
+++ test/node/home-blog-posts.js
@@ -14,7 +14,7 @@ describe('Test Home Page + Blog Posts', () => {
   let serverUrl;
 
   before(function() {
-    this.timeout(120 * 1000);
+    this.timeout(5 * 60 * 1000);
 
     return Promise.all([
       siteServer.start(desiredPort),
