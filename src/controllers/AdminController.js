8987542084c9acf17418a8a328c1cdce1d672a91
Matt Gaunt <matt@gauntface.co.uk>
2018-02-02 08:00:01 -0800

    Adding very start of admin

---
+++ src/controllers/AdminController.js
@@ -0,0 +1,50 @@
+
+const isLoggedIn = () => {
+  return false;
+}
+
+const assertSignedIn = (request) => {
+  if (isLoggedIn() || request.url === '/admin/signin') {
+    return;
+  }
+
+  if (request.method === 'post') {
+    throw new Error(`API should return a 403 error.`);
+  } else {
+    request.res.redirect(`/admin/signin`);
+  }
+};
+
+class AdminController {
+  index(args) {
+    assertSignedIn(args.request);
+
+    return {
+      data: {
+        title: 'GauntFace | Matthew Gaunt - Blog',
+      },
+      templatePath: 'templates/documents/html.tmpl',
+      views: [
+        {
+          templatePath: 'templates/views/admin/index.tmpl',
+        },
+      ],
+    };
+  }
+
+  signin(args) {
+    return {
+      data: {
+        title: 'GauntFace | Matthew Gaunt - Blog',
+      },
+      templatePath: 'templates/documents/html.tmpl',
+      views: [
+        {
+          templatePath: 'templates/views/admin/signin.tmpl',
+        },
+      ],
+    };
+  }
+}
+
+module.exports = new AdminController();
